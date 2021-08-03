9432e8db6316f15c81c3d4fa26e5523dad427c6b
Gabriel Cassanta <cassantagabriel@gmail.com>
2021-08-03 11:25:33 -0300

    DateHelper, ListaNegociacoes, NegociacacoController

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,14 +6,31 @@ class NegociacaoController {
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
+    this._listaNegociacoes = new ListaNegociacoes();
   }
 
   adiciona(event) {
     event.preventDefault();
 
-    let data = new Date(this._inputData.value.split('-'))
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    this._limpaFormulario();
 
-    console.log(data);
-    // adicionar a negociação em uma lista
+    console.log(this._listaNegociacoes.negociacoes);
+  }
+
+  _criaNegociacao() {
+
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
+      this._inputQuantidade.value,
+      this._inputValor.value);
+  }
+
+  _limpaFormulario() {
+
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0;
+    this._inputData.focus();
   }
 }
